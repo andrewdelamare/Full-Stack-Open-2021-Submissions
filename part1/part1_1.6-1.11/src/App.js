ff4ecab26c018de0f6e13262b1d2331912d86b17
Andrew de Lamare <80968376+andrewdelamare@users.noreply.github.com>
2022-01-19 12:44:36 +0200

    1.6 complete

--- part1/part1_1.6-1.11/src/App.js
+++ part1/part1_1.6-1.11/src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Button = (props) => {
+  return(<button onClick={props.setType}>{props.feedback}</button>)
 }
 
-export default App;
+const Feedback = (props) => {
+  return (
+  <div>
+    <Button setType={props.setGood} feedback={props.good}/>
+    <Button setType={props.setNeutral} feedback={props.neutral} />
+    <Button setType={props.setBad} feedback={props.bad} />
+  </div>)
+}
+
+const Statistics = (props) => {
+
+  return(
+    <div>
+      <p>Good: {props.good} </p>
+      <p>Neutral: {props.neutral} </p>
+      <p>Bad: {props.bad} </p>
+    </div>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  
+  const handleGood = () => {
+    setGood(good + 1) 
+  }
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBad = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <h2>Give Feedback</h2>
+      <Feedback setGood={handleGood} setNeutral={handleNeutral} setBad={handleBad} good="good" neutral='neutral' bad='bad' />
+      <h2>Statistics</h2>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+export default App
