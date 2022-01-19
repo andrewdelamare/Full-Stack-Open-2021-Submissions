32a4268b1cdeb36b450492fd11da69d9d8b6f8f4
Andrew de Lamare <80968376+andrewdelamare@users.noreply.github.com>
2022-01-19 13:40:14 +0200

    part 1.7 & 1.8

--- part1/part1_1.6-1.11/src/App.js
+++ part1/part1_1.6-1.11/src/App.js
@@ -1,5 +1,18 @@
 import React, { useState } from 'react'
 
+
+const Average = (props) => {
+  const {good, neutral, bad} = props
+  let score = good - bad 
+  let total = good + bad + neutral
+  let ave = score/total
+  if (isNaN(ave)){
+    return(
+      <p>Average: 0</p>)
+  }else{
+    return(<p>Average: {ave}</p>)}
+}
+
 const Button = (props) => {
   return(<button onClick={props.setType}>{props.feedback}</button>)
 }
@@ -13,6 +26,17 @@ const Feedback = (props) => {
   </div>)
 }
 
+const Positive = (props) => {
+  const {good, neutral, bad} = props
+  const total = good + neutral + bad
+  const ratio = good/total 
+  const percent = ratio * 100
+  if (isNaN(percent)){
+    return(<p>Positive: </p>)
+  }else{
+    return(<p>Positive: {percent}%</p>)}
+}
+
 const Statistics = (props) => {
 
   return(
@@ -20,6 +44,8 @@ const Statistics = (props) => {
       <p>Good: {props.good} </p>
       <p>Neutral: {props.neutral} </p>
       <p>Bad: {props.bad} </p>
+      <Average good={props.good} neutral={props.neutral} bad={props.bad} />
+      <Positive good={props.good} neutral={props.neutral} bad={props.bad} />
     </div>
   )
 }
@@ -29,6 +55,7 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
   
   const handleGood = () => {
     setGood(good + 1) 
@@ -41,6 +68,7 @@ const App = () => {
   const handleBad = () => {
     setBad(bad + 1)
   }
+
 
   return (
     <div>
