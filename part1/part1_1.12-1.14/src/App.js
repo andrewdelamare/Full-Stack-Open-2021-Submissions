b3a5cf91368836584770fc009d2791c99b0d43f6
Andrew de Lamare <80968376+andrewdelamare@users.noreply.github.com>
2022-01-20 12:12:39 +0200

    1.12 anecdotes step 1

--- part1/part1_1.12-1.14/src/App.js
+++ part1/part1_1.12-1.14/src/App.js
@@ -1,14 +1,43 @@
 import React, { useState } from 'react'
 
+const Anecdote = (props) => {
+  return(
+  <div>
+    {props.anec[props.select]}
+  </div>)
+}
+
+const Button = (props) => {
+  return(<button onClick={props.onClick}>Next Anecdote</button>)
+}
+
+const randomAnecdote = () => {
+  let rand = Math.floor(Math.random()*7)
+  return(rand)
+}
+
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const handleButton = () => {
+    setSelected(randomAnecdote())
+  }
 
   return (
     <div>
-      code here
+      <Anecdote anec={anecdotes} select={selected} />
+      <Button onClick={handleButton}/>
+      
+      
     </div>
   )
 }
