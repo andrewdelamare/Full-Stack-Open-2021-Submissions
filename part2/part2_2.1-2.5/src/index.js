efb77b4797d5542d22c16c2ea84ce4f4ddadba0d
Andrew de Lamare <80968376+andrewdelamare@users.noreply.github.com>
2022-01-24 14:10:40 +0200

    2.5 done

--- part2/part2_2.1-2.5/src/index.js
+++ part2/part2_2.1-2.5/src/index.js
@@ -1,47 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const Header = ({ course }) => {
-  return (
-    <h1>{course.name}</h1>
-  )
-}
-
-const Total = ({ course }) => {
-  const parts = course.parts
-  const exerciseList = parts.map((part) => part.exercises)
-  const sum = exerciseList.reduce((total, exercises)=> total+=exercises, 0)
-  return(
-    <p>Total number of exercises: {sum}</p>
-  ) 
-}
-
-const Part = (props) => {
-  return (
-    <p key={props.id}>
-      {props.name}: {props.exercises}
-    </p>    
-  )
-}
-
-const Content = ({ course }) => {
-  const parts = course.parts
-  return (
-    <div>
-      {parts.map((part) => <Part key={part.id} name={part.name} exercises={part.exercises} />)}
-    </div>
-  )
-}
-
-const Course = ({ course }) => {
-  return(
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>
-  )
-} 
+import { Course } from './components';
 
 const App = () => {
   const courses = [
