91f7de0db534c02eca00f3d21b9346ef77069fbe
Andrew de Lamare <80968376+andrewdelamare@users.noreply.github.com>
2022-01-25 11:05:20 +0200

    2.6 done

--- part2/part2_Phonebook/phonebook/src/App.js
+++ part2/part2_Phonebook/phonebook/src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const Part = (props) => {
+  console.log(props)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <p key={props.key}>
+      {props.name}
+    </p>    
+  )
 }
 
-export default App;
+const Content = ({ persons }) => {
+  console.log(persons)
+  return (
+    <div>
+      {persons.map((person) => <Part key={person.id} name={person.name} />)}
+    </div>
+  )
+}
+
+const Numbers = ({ persons }) => {
+  return(
+    <div>
+      <h2>Numbers</h2>
+      <Content persons={persons} />
+   </div> 
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas',
+      id: 0 }
+  ]) 
+  const [newName, setNewName] = useState('')
+  
+
+  const addNewPerson = (event) => {
+    event.preventDefault()
+    const oldPersons = persons
+    const id = oldPersons.length 
+    const newPerson = {name: newName, id: id}
+    const newPersons = oldPersons.concat(newPerson)
+    setPersons(newPersons)
+  }
+
+  const handleNewName = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+    console.log(persons)
+  }
+
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNewPerson}>
+        <div>
+          name: <input onChange={handleNewName} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <Numbers persons={persons} />
+    </div>
+  )
+}
+
+export default App
