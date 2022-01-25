3b8534a27065950c68fbe068bb150364d95b6f8d
Andrew de Lamare <80968376+andrewdelamare@users.noreply.github.com>
2022-01-25 13:10:45 +0200

    2.8 done

--- part2/part2_Phonebook/phonebook/src/App.js
+++ part2/part2_Phonebook/phonebook/src/App.js
@@ -4,7 +4,7 @@ const Part = (props) => {
   console.log(props)
   return (
     <p key={props.id}>
-      {props.name}
+      {props.name} {props.number}
     </p>    
   )
 }
@@ -13,7 +13,7 @@ const Content = ({ persons }) => {
   console.log(persons)
   return (
     <div>
-      {persons.map((person) => <Part key={person.id} name={person.name} />)}
+      {persons.map((person) => <Part key={person.id} name={person.name} number={person.number} />)}
     </div>
   )
 }
@@ -29,10 +29,11 @@ const Numbers = ({ persons }) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
+    { name: 'Arto Hellas', number: '650 123 4567',
       id: 0 }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
   
 
   const addNewPerson = (event) => {
@@ -44,7 +45,7 @@ const App = () => {
     if(included){
       return(window.alert(`${newName} is already indluded in your phonebook`))
     }else{
-    const newPerson = {name: newName, id: id}
+    const newPerson = {name: newName, number: newNumber, id: id}
     const newPersons = oldPersons.concat(newPerson)
     setPersons(newPersons)}
   }
@@ -55,13 +56,22 @@ const App = () => {
     console.log(persons)
   }
 
+  const handleNewNumber = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+    console.log(persons)
+  }
+
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addNewPerson}>
         <div>
-          name: <input onChange={handleNewName} />
+          Name: <input onChange={handleNewName} />
+        </div>
+        <div>
+          Number: <input onChange={handleNewNumber} />
         </div>
         <div>
           <button type="submit">add</button>
