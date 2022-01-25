669c4399d3746040bcc58f8ee4ed8f3896a8c580
Andrew de Lamare <80968376+andrewdelamare@users.noreply.github.com>
2022-01-25 15:15:36 +0200

    2.9 case insensitivie filtering done

--- part2/part2_Phonebook/phonebook/src/App.js
+++ part2/part2_Phonebook/phonebook/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
 const Part = (props) => {
-  console.log(props)
+  
   return (
     <p key={props.id}>
       {props.name} {props.number}
@@ -9,32 +9,53 @@ const Part = (props) => {
   )
 }
 
-const Content = ({ persons }) => {
-  console.log(persons)
-  return (
+const Content = ({ persons, filter }) => {
+  if(filter.isOn){
+    const filtered = persons.filter(person => person.name.toLowerCase().includes(filter.text.toLowerCase()))
+    return (
+      <div>
+        {filtered.map((person) => <Part key={person.id} name={person.name} number={person.number} />)}
+      </div>
+    )
+  }else{
+    return (
     <div>
       {persons.map((person) => <Part key={person.id} name={person.name} number={person.number} />)}
     </div>
-  )
+  )}
 }
 
-const Numbers = ({ persons }) => {
+const Numbers = ({ persons, filter }) => {
   return(
     <div>
       <h2>Numbers</h2>
-      <Content persons={persons} />
+      <Content persons={persons} filter={filter} />
    </div> 
+  )
+}
+
+const Filter = (props) => {
+  return(
+    <div>
+      <form>
+        <div>
+          Filter with: <input onChange={props.handleNewFilter} />
+        </div>
+      </form>
+    </div>
   )
 }
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '650 123 4567',
-      id: 0 }
+    { name: 'Arto Hellas', number: '650 123 4567', id: 0 }, 
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 1 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 2 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 3 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-  
+  const [filter, setNewFilter] = useState({text: '', isOn: false})
 
   const addNewPerson = (event) => {
     event.preventDefault()
@@ -51,21 +72,24 @@ const App = () => {
   }
 
   const handleNewName = (event) => {
-    console.log(event.target.value)
     setNewName(event.target.value)
-    console.log(persons)
   }
 
   const handleNewNumber = (event) => {
-    console.log(event.target.value)
     setNewNumber(event.target.value)
-    console.log(persons)
   }
 
+  const handleNewFilter = (event) => {
+    const value = event.target.value
+    value.toLowerCase()
+    let on = value !== '' ? true : false
+    setNewFilter({text: `${value}`, isOn:on })
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Filter handleNewFilter={handleNewFilter} />
       <form onSubmit={addNewPerson}>
         <div>
           Name: <input onChange={handleNewName} />
@@ -77,7 +101,7 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
-      <Numbers persons={persons} />
+      <Numbers persons={persons} filter={filter} />
     </div>
   )
 }
