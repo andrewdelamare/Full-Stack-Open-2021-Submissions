0d3e7aad785b04fd79f14fff9385a21ab6ca8ba4
Andrew de Lamare <80968376+andrewdelamare@users.noreply.github.com>
2022-02-09 11:42:45 +0200

    3.16 error handling done

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -4,94 +4,85 @@ const express = require('express')
 const res = require('express/lib/response')
 const morgan = require('morgan')
 const app = express()
+app.use(express.json())
 const cors = require('cors')
 const Person = require( './modules/persons')
-app.use(express.json())
 app.use(cors())
 morgan.token('people', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :people'))
 
-let persons = []
+let date = new Date
+let count = undefined
 
-const total = () => {
-  let count = 0
-  persons.forEach(() => count += 1)
-  return(count)
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 
-let date = new Date
 
-app.get('/api/persons', (request, response) => {
-  console.log(`people before get: ${persons}`)
+
+app.get('/api/persons', (request, response, next) => {
   Person.find({})
-  .then(peep => {
-    response.json(peep)
-    peep.forEach(p => persons.concat(p))
-    console.log(`people after get: ${persons}`)
-  })
+  .then(peep => response.json(peep))
+  .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
   const person = new Person({
     name: body.name,
     number: body.number
   })
-   if(!person){
-    return(response.status(400).json({error: 'content missing'}))
-  }else if(!person.name && person.number){
-    return(response.status(400).json({error: 'name missing'}))
-  }else if(!person.number && person.name){
-    return(response.status(400).json({error: 'number missing'}))
-  }else if(!person.name && !person.number){
-    return(response.status(400).json({error: 'content missing'}))
-  }else{
-  //  person.id = Math.floor(Math.random()*100000)
-    persons = persons.concat(person)
-    console.log(persons)
-    person.save().then(savedPerson => {
-      response.json(savedPerson)
-    })
-  }
-})
-
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  console.log(`Removing entry with id: ${id}`)
-  persons = persons.filter(person => person.id !== id)
-  Person.findByIdAndRemove(id).then(response.status(204).end())
-  
-  
-})
-
-app.get('/api/persons/:id', (request, response) => {
-  const wanted = Person.findById(request.params.id).then(person => {
-    response.json(person)
+  person.save()
+  .then(savedPerson => {
+    response.json(savedPerson)
   })
-  if(wanted){
-    response.json(wanted)
-    response.status(200).end()
-  }else{
-    response.status(404).end()
-  }
+  .catch(error => next(error))
 })
 
-app.put('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
+  const id = request.params.id
+  Person.findByIdAndRemove(id)
+  .then(response.status(204).end())
+  .catch(error => next(error))
+  console.log(`Removing entry with id: ${id}`)
+})
+
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+  .then(
+    person => {
+      response.json(person)
+      response.status(200).end()
+    })
+  .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
   const id = request.params.id
   const update = request.params.number.toString()
   
-  if(Person.findById(id)){
-    Person.findByIdAndUpdate(id, update) 
-    response.status(200).end()
-  }else{
-    console.log("NOT FOUND")
-    response.status(404).end()
-  }
+  Person.findByIdAndUpdate(id, update)
+  .then(response.status(200).end())
+  .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
-  response.send(`<p> Phonebook has info for ${total()} people </p> <p>${date}</p>`)
+
+  response.send(`<p> Phonebook has info for _ people </p> <p>${date}</p>`)
 })
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
+
 
 const PORT = process.env.PORT 
 app.listen(PORT, () => {
