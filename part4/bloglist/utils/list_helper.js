f2c99b59d8fce64ca8a68cf362f471fe903c4ab5
Andrew de Lamare <80968376+andrewdelamare@users.noreply.github.com>
2022-02-15 14:05:55 +0200

    4.5 done

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,13 +2,31 @@ const dummy = () => 1;
 
 const totalLikes = (blogs) => {
   let total = 0;
-  blogs.forEach(blog => {
+  blogs.forEach((blog) => {
     total += blog.likes;
   });
   return total;
 };
 
+const favoriteBlog = (blogs) => {
+  let fav = {
+    _id: '5a422ba71b54a676234d17fb',
+    title: 'TDD harms architecture',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+    likes: 0,
+    __v: 0,
+  };
+  blogs.forEach((blog) => {
+    if (blog.likes > fav.likes) {
+      fav = blog;
+    }
+  });
+  return fav;
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
