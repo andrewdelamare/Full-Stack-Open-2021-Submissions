14390c8149cf834b8de5fa95df15192803b6a46a
Andrew de Lamare <80968376+andrewdelamare@users.noreply.github.com>
2022-02-22 15:30:07 +0200

    4.16 done

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -4,19 +4,28 @@ const User = require('../models/user');
 
 usersRouter.post('/api/users', async (request, response) => {
   const { username, name, password } = request.body;
+  const existingUsers = await User.find({ username: `${username}` });
+  const isFound = existingUsers.length;
+  if (username.length <= 2) {
+    response.status(400).json({ error: 'username too short' });
+  } else if (password.length <= 2) {
+    response.status(400).json({ error: 'password too short' });
+  } else if (isFound >= 1) {
+    response.status(400).json({ error: 'username already exists' });
+  } else if (isFound === 0) {
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(password, saltRounds);
 
-  const saltRounds = 10;
-  const passwordHash = await bcrypt.hash(password, saltRounds);
+    const user = new User({
+      username,
+      name,
+      passwordHash,
+    });
 
-  const user = new User({
-    username,
-    name,
-    passwordHash,
-  });
+    const savedUser = await user.save();
 
-  const savedUser = await user.save();
-
-  response.status(201).json(savedUser);
+    response.status(201).json(savedUser);
+  }
 });
 
 usersRouter.get('/api/users', async (request, response) => {
--- part4/bloglist/tests/users.test.js
+++ part4/bloglist/tests/users.test.js
@@ -5,3 +5,86 @@ const User = require('../models/user');
 
 const api = supertest(app);
 
+const initialUsers = [
+  {
+    username: 'testUser',
+    name: 'I do not exist',
+    password: 'fake',
+  },
+  {
+    username: 'testUser2',
+    name: 'I also do not exist',
+    password: 'fake2',
+  },
+];
+
+const usrShort = {
+  username: 'te',
+  name: 'I also do not exist',
+  password: 'fake',
+};
+
+const usrMissing = {
+  username: '',
+  name: 'I also do not exist',
+  password: 'fake',
+};
+
+const usrExists = {
+  username: 'testUser',
+  name: 'I do not exist',
+  password: 'fake2',
+};
+
+const passShort = {
+  username: 'passShort',
+  name: 'I do not exist',
+  password: 'fa',
+};
+
+const passMissing = {
+  username: 'passMissing',
+  name: 'I also do not exist',
+  password: '',
+};
+
+beforeEach(async () => {
+  await User.deleteMany({});
+  let userObject = new User(initialUsers[0]);
+  await userObject.save();
+  userObject = new User(initialUsers[1]);
+  await userObject.save();
+});
+
+describe('User', () => {
+  test('is not saved if username too short', async () => {
+    await api
+      .post('/api/users')
+      .send(usrShort)
+      .expect(400);
+  });
+  test('is not saved if username missing', async () => {
+    await api
+      .post('/api/users')
+      .send(usrMissing)
+      .expect(400);
+  });
+  test('is not saved if username exists', async () => {
+    await api
+      .post('/api/users')
+      .send(usrExists)
+      .expect(400);
+  });
+  test('is not saved if password is too short', async () => {
+    await api
+      .post('/api/users')
+      .send(passShort)
+      .expect(400);
+  });
+  test('is not saved if password is missing', async () => {
+    await api
+      .post('/api/users')
+      .send(passMissing)
+      .expect(400);
+  });
+});
