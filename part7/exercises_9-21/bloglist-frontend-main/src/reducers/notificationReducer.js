bb5f260b77c7459f33845d2c0bb2167a53e311ef
Andrew de Lamare <80968376+andrewdelamare@users.noreply.github.com>
2022-04-08 13:18:32 +0300

    some initial redux setup for 7.10, not done yet

---
+++ part7/exercises_9-21/bloglist-frontend-main/src/reducers/notificationReducer.js
@@ -0,0 +1,42 @@
+import {createSlice} from '@reduxjs/toolkit';
+import {useSelector} from 'react-redux';
+
+const initialState = '';
+
+const notificationSlice = createSlice({
+  name: 'notification',
+  initialState,
+  reducers: {
+    notify(state, action) {
+      const content = action.payload;
+
+      state = content;
+
+      return state;
+    },
+    clear(state) {
+      state = '';
+      return state;
+    },
+  },
+});
+
+export const {notify, clear} = notificationSlice.actions;
+
+export const displayNotification = (note, time) => {
+  return async (dispatch) => {
+    const highestId = window.setTimeout(() => {
+      for (let i = highestId; i >= 0; i--) {
+        window.clearInterval(i);
+      }
+    }, 0);
+    dispatch(notify(note));
+    setTimeout(async ()=> {
+      console.log('clearing notification!!!');
+      dispatch(clear());
+    }, time*1000);
+  };
+};
+
+
+export default notificationSlice.reducer;
