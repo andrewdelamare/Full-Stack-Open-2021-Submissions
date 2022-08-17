ee26c3d47abaaa1d142e98ca75bf0f622bca4eb9
Andrew de Lamare <80968376+andrewdelamare@users.noreply.github.com>
2022-08-17 11:00:53 +0300

    9.9 done

--- part9/patientor/index.ts
+++ part9/patientor/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
+import cors from "cors";
 const app = express();
 app.use(express.json());
-app.get("/ping", (_req, res) => {
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
+app.get("/api/ping", (_req, res) => {
   res.send("pong");
 });
 
-const PORT = 3002;
+const PORT = 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
