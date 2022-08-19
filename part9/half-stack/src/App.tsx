d50c5663fc2a4763901b3c4008adc2cf69edf6c7
Andrew de Lamare <80968376+andrewdelamare@users.noreply.github.com>
2022-08-19 13:40:13 +0300

    9.15 done

--- part9/half-stack/src/App.tsx
+++ part9/half-stack/src/App.tsx
@@ -2,27 +2,65 @@ import { Content } from "./Content";
 import { Header } from "./Header";
 import { Total } from "./Total";
 
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+  type: string;
+}
+
+interface CoursePartDesc extends CoursePartBase {
+  description: string
+}
+interface CourseNormalPart extends CoursePartDesc {
+  type: "normal";
+}
+interface CourseProjectPart extends CoursePartBase {
+  type: "groupProject";
+  groupProjectCount: number;
+}
+
+interface CourseSubmissionPart extends CoursePartDesc {
+  type: "submission";
+  exerciseSubmissionLink: string;
+}
+
+export type CoursePart = CourseNormalPart | CourseProjectPart | CourseSubmissionPart;
+
 const App = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
-    {
-      name: "Fundamentals",
-      exerciseCount: 10
-    },
-    {
-      name: "Using props to pass data",
-      exerciseCount: 7
-    },
-    {
-      name: "Deeper type usage",
-      exerciseCount: 14
-    }
-  ];
+// this is the new coursePart variable
+const courseParts: CoursePart[] = [
+  {
+    name: "Fundamentals",
+    exerciseCount: 10,
+    description: "This is the easy course part",
+    type: "normal"
+  },
+  {
+    name: "Advanced",
+    exerciseCount: 7,
+    description: "This is the hard course part",
+    type: "normal"
+  },
+  {
+    name: "Using props to pass data",
+    exerciseCount: 7,
+    groupProjectCount: 3,
+    type: "groupProject"
+  },
+  {
+    name: "Deeper type usage",
+    exerciseCount: 14,
+    description: "Confusing description",
+    exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev",
+    type: "submission"
+  }
+]
 
   return (
     <div>
       <Header title={courseName} />
-      {courseParts.map(c => <Content key={c.name.length} name={c.name} count={c.exerciseCount} />)}
+      <Content arr={courseParts} />
       <Total courseParts={courseParts} />
     </div>
   );
--- part9/half-stack/src/Content.tsx
+++ part9/half-stack/src/Content.tsx
@@ -1,7 +1,42 @@
-export const Content = ({name, count}: {name: string, count: number}) => {
-  return (
-    <p>
-      {name} {count}
-    </p>
-  )
+/* eslint-disable react/jsx-key */
+import { CoursePart } from "./App"
+
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
+  );
+};
+
+export const Content = ({arr}: {arr: CoursePart[]}) => {
+  const partArr = arr.map(a => {
+    switch (a.type) {
+      case "normal":
+        return (
+        <div>
+          <h2>{a.name} {a.exerciseCount }</h2>
+          <p>{a.description}</p>
+        </div>)
+      case "groupProject":
+        return (
+        <div>
+          <h2>{a.name} {a.exerciseCount }</h2>
+          <p>project exercises {a.groupProjectCount}</p>
+        </div>)
+      case "submission":
+        return (
+        <div>
+          <h2>{a.name} {a.exerciseCount }</h2>
+          <p>{a.description}</p>
+          <p>{a.exerciseSubmissionLink}</p>
+        </div>)
+      default:
+        assertNever(a)
+        break;
+    }})
+
+return( 
+<div>
+  {partArr.map(p => p )}
+</div>
+)  
 }
