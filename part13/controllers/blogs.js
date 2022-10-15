32b689abde19beba33ff83cc6a1a11f157e5afdf
Andrew de Lamare <80968376+andrewdelamare@users.noreply.github.com>
2022-10-15 15:46:05 +0300

    13.14 done

--- part13/controllers/blogs.js
+++ part13/controllers/blogs.js
@@ -7,10 +7,22 @@ router.get('/', async (req, res) => {
   let where = {};
 
   if (req.query.search) {
-    where.title = {
-      [Op.substring]: req.query.search       
+    where = {
+      [Op.or]: [
+        {
+          title: {
+            [Op.substring]: req.query.search       
+          }
+        },
+        {
+          author: {
+            [Op.substring]: req.query.search       
+          }
+        }
+      ]
     }
   }
+
 
   const blogs = await Blog.findAll({
     attributes: { exclude: ['userId'] },
